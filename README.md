ea981e2a4b2881d09050710b61bc220ccaf7aa1a
JJ Philipp <jj@moov.io>
2025-01-24 17:02:30 -0600

    Tweaking more stuff to clean it up

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ The samples below show how a published SDK artifact is used:
 
 Gradle:
 ```groovy
-implementation 'io.moov:sdk:0.1.6'
+implementation 'io.moov:sdk:0.3.0'
 ```
 
 Maven:
@@ -58,7 +58,7 @@ Maven:
 <dependency>
     <groupId>io.moov</groupId>
     <artifactId>sdk</artifactId>
-    <version>0.1.6</version>
+    <version>0.3.0</version>
 </dependency>
 ```
 
--- src/main/java/io/moov/sdk/SDKConfiguration.java
+++ src/main/java/io/moov/sdk/SDKConfiguration.java
@@ -33,7 +33,7 @@ class SDKConfiguration {
     public int serverIdx = 0;
     private static final String LANGUAGE = "java";
     public static final String OPENAPI_DOC_VERSION = "latest";
-    public static final String SDK_VERSION = "0.1.6";
+    public static final String SDK_VERSION = "0.3.0";
     public static final String GEN_VERSION = "2.496.0";
     private static final String BASE_PACKAGE = "io.moov.sdk";
     public static final String USER_AGENT = 
